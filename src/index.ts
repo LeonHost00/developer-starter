14c2f37c621d29f812ac9309b992c2fa482828ce
LeonHost00 <139167371+LeonHost00@users.noreply.github.com>
2023-07-10 23:12:23 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
-import { greetUser } from '$utils/greet';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const calendarElement = document.querySelector<HTMLDivElement>('[data-element="calendar"]');
+  if (!calendarElement) return;
+
+  console.log({ calendarElement });
+
+  const calendar = new Calendar(calendarElement, {
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin, googleCalendarPlugin],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,listWeek'
+    },
+    googleCalendarApiKey: 'AIzaSyClISbFHnM2Uqvpot8Bwzs44zZ3zDtIBzY',
+    events: {
+      googleCalendarId: 'info@webbyuf.com',
+      className: 'gcal-event' // an option!
+    },
+
+
+  });
+
+  calendar.render();
 });
